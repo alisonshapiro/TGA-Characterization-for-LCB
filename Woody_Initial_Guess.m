9b6ef5cc9c0e3d8551c021e0edf0d5b8130103ed
alisonshapiro <136035729+alisonshapiro@users.noreply.github.com>
2023-10-17 14:21:30 -0400

    Update Woody_Initial_Guess.m

--- Woody_Initial_Guess.m
+++ Woody_Initial_Guess.m
@@ -1,5 +1,5 @@
 % Use this file first to visually determine a reasonable initial guess for
-% all 12 parameters, then paste findings into Woody_model.m and run
+% all 15 parameters, then paste findings into Woody_model.m and run
 
 % Import data from excel into MATLAB
 [Temp] = readmatrix('Lignin_TGA_data.xlsx','Range','A:A'); %(C)
@@ -9,14 +9,17 @@
 abs_D = abs(Deriv);
 
 % Initial guess of parameters
+s0 = 10.4715;
+m0 = 290.773;
+a0 = 10.81441;
 s1 = 20; %sigma of Guassian 1, width of distribution
-m1 = 310.11; %mu of Guassian 1, middle of distribution
-a1 = 30.2562; %scale of Guassian 1, height of distribution
+m1 = 320.11; %mu of Guassian 1, middle of distribution
+a1 = 20.2562; %scale of Guassian 1, height of distribution
 s2 = 5.57045;
-m2 = 340.04;
-a2 = 30.8889;
+m2 = 350.04;
+a2 = 15.8889;
 s3 = 30.7094;
-m3 = 400.901;
+m3 = 420.901;
 a3 = 13.0202;
 s4 = 20.4715;
 m4 = 470.773;
@@ -24,6 +27,7 @@ a4 = 10.81441;
 
 % Initialize arrays
 l = length(Temp);
+G0 = zeros(1,1);
 G1 = zeros(l,1);
 G2 = zeros(l,1);
 G3 = zeros(l,1);
@@ -31,8 +35,9 @@ G4 = zeros(l,1);
 fit = zeros(l,1);
 sq_err = zeros(l,1);
 
-v = [s1; m1; a1; s2; m2; a1; s3; m3; a3; s4; m4; a4];
+v = [s0; m0; a0; s1; m1; a1; s2; m2; a1; s3; m3; a3; s4; m4; a4];
 for i = 1:length(Temp)
+    G0(i) = a0 / (s0 * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - m0) / s0)^2);
     G1(i) = a1 / (s1 * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - m1) / s1)^2);
     G2(i) = a2 / (s2 * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - m2) / s2)^2);
     G3(i) = a3 / (s3 * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - m3) / s3)^2);
@@ -45,6 +50,7 @@ RMSE = sqrt(mean(sq_err));
 % Plot curves
 plot(Temp, abs_D, 'Color', 'm', 'LineWidth', 2)
 hold on
+plot(Temp, G0, 'Color', 'c', 'LineWidth', 2)
 plot(Temp, G1, 'Color', 'r', 'LineWidth', 2)
 plot(Temp, G2, 'Color', 'b', 'LineWidth', 2)
 plot(Temp, G3, 'Color', 'g', 'LineWidth', 2)
@@ -53,8 +59,9 @@ plot(Temp, fit, 'LineStyle', '--', 'Color', 'k', 'LineWidth', 2)
 xlabel('Temperature (°C)')
 ylabel('Derivative (wt. %/°C)')
 
-legend('Experimental','Holocellulose 1', 'Holocellulose 2','Lignin 1','Lignin 2','Fit')
+legend('Experimental','Hemicellulose','Cellulose 1', 'Cellulose 2','Lignin 1','Lignin 2','Fit')
 legend('boxoff')
 axis([150,600,0,2.5])
 title('Initial Parameter Guess')
 set(gcf,'color','white')
+
