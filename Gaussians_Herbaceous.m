f986fc4c9d0f017a4d6d6c126b5bf08033e5d70b
alisonshapiro <136035729+alisonshapiro@users.noreply.github.com>
2023-10-17 14:32:40 -0400

    Update Gaussians_Herbaceous.m

--- Gaussians_Herbaceous.m
+++ Gaussians_Herbaceous.m
@@ -5,6 +5,7 @@ l = length(Temp);
 G1 = zeros(l,1);
 G2 = zeros(l,1);
 G3 = zeros(l,1);
+G4 = zeros(l,1);
 fit = zeros(l,1);
 sq_err = zeros(l,1);
 
@@ -12,12 +13,12 @@ sq_err = zeros(l,1);
         G1(i) = v(3) / (v(1) * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - v(2)) / v(1))^2);
         G2(i) = v(6) / (v(4) * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - v(5)) / v(4))^2);
         G3(i) = v(9) / (v(7) * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - v(8)) / v(7))^2);
-        fit(i) = G1(i) + G2(i) + G3(i);
+        G4(i) = v(12) / (v(10) * sqrt(2*pi)) * exp(-0.5 * ((Temp(i) - v(11)) / v(10))^2);
+        fit(i) = G1(i) + G2(i) + G3(i) + G4(i);
         sq_err(i) = (fit(i) - abs_D(i))^2;
     end
 RMSE = sqrt(mean(sq_err));
 end
-
 
 
 
